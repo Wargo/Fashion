4e2c909fc6b51296b7222892a7f163ae55f29e9e
Guillermo <guillermo@artvisual.net>
2012-09-05 17:54:25 +0200

    maravillas que sé hacer :D

--- Resources/bbdd/getData.js
+++ Resources/bbdd/getData.js
@@ -1,14 +1,16 @@
 
-module.exports = function(getImages) {
+module.exports = function(getImages, page) {
 	
 	var path = Ti.App.dataURL + 'my.php';
 	var client = Ti.Network.createHTTPClient({
 		onload: function() {
 			var result = eval('(' + this.responseText + ')');
 			if (result.status === 'ok') {
-				getImages(result.data);
+				getImages(result.data, page);
 			} else {
-				Ti.App.alert(L('txt_error'), result.message);
+				if (result.message) {
+					Ti.App.alert(L('txt_error'), result.message);
+				}
 				getImages(null);
 			}
 		},
@@ -20,7 +22,8 @@ module.exports = function(getImages) {
 	
 	client.open('POST', path);
 	client.send({
-		id:Ti.App.Properties.getInt('user_id')
+		id:Ti.App.Properties.getInt('user_id'),
+		page:page
 	});
 	
 }
--- Resources/myPhotos.js
+++ Resources/myPhotos.js
@@ -1,4 +1,8 @@
 module.exports = function() {
+	
+	var images = [];
+	var editing = false;
+	var canEdit = false;
 
 	var win = Ti.UI.createWindow({
 		backgroundImage:'images/background.png',
@@ -14,6 +18,66 @@ module.exports = function() {
 	backButton.addEventListener('click', function() {
 		win.close({left:400});
 	});
+	
+	var separator = Ti.UI.createView({
+		width:1,
+		backgroundColor:'#BFB8B2',
+		right:60
+	});
+	var separator2 = Ti.UI.createView({
+		width:1,
+		backgroundColor:'#BFB8B2',
+		left:50
+	});
+	
+	var editButton = Ti.UI.createView({
+		width:60,
+		height:40,
+		right:0,
+		opacity:0.4
+	});
+	editButton.add(Ti.UI.createLabel({text:L('edit')}));
+	editButton.addEventListener('click', function() {
+		if (!canEdit) {
+			return;
+		}
+		if (editing) {
+			win.close({left:400});
+		}
+		editing = true;
+		var tr1 = Ti.UI.create2DMatrix({
+			rotate:-2
+		});
+		var tr2 = Ti.UI.create2DMatrix({
+			rotate:2
+		});
+		
+		for (x in images) {
+			
+			eval('var animation1_' + x + ' = Ti.UI.createAnimation({transform:tr1, duration:100});');
+			eval('var animation2_' + x + ' = Ti.UI.createAnimation({transform:tr2, duration:100});');
+			
+			var animation1_x = eval('animation1_' + x);
+			var animation2_x = eval('animation2_' + x);
+		
+			images[x].animate(animation1_x);
+			
+			animation1_x._x = x;
+			animation2_x._x = x;
+			
+			animation1_x.addEventListener('complete', function(e) {
+				currentX = e.source._x;
+				var animation2_x = eval('animation2_' + currentX);
+				images[currentX].animate(animation2_x);
+			});
+			animation2_x.addEventListener('complete', function(e) {
+				currentX = e.source._x;
+				var animation1_x = eval('animation1_' + currentX);
+				images[currentX].animate(animation1_x);
+			});
+			
+		}
+	});
 
 	var header = Ti.UI.createView({
 		backgroundColor:'#F2F2F2',
@@ -23,24 +87,30 @@ module.exports = function() {
 	});
 	header.add(Ti.UI.createLabel({text:L('title_my_photos')}));
 	header.add(backButton);
+	header.add(editButton);
+	header.add(separator);
+	header.add(separator2);
 	
-	var scrollView = Ti.UI.createScrollView({
-		showVerticalScrollIndicator:true,
-		contentHeight:'auto',
-		top:40
+	var scrollableView = Ti.UI.createScrollableView({
+		showPagingControl:true,
+		pagingControlColor:'transparent'
 	});
 	
 	var loading = Ti.UI.createActivityIndicator();
 	win.add(loading);
 	loading.show();
 	
-	function getImages(data) {
+	function getImages(data, page) {
 		if (data === null) {
-			win.close({left:400});
+			//win.close({left:400});
+			return;
 		}
+		
+		var view = Ti.UI.createView({top:40})
+		
 		var top = 10;
 		for (i in data) {
-			var left = 10 + (i % 3) * 100;
+			var left = 15 + (i % 3) * 100;
 			if (i % 3 === 0 && i != 0) {
 				top += 140;
 			}
@@ -78,26 +148,8 @@ module.exports = function() {
 					e.source._smallLoading.hide();
 				}
 			});
-
-			img.addEventListener('longpress', function(e) {
-				var tr1 = Ti.UI.createTransition({
-					rotate:-5
-				});
-				var tr2 = Ti.UI.createTransition({
-					rotate:5
-				});
-				var animate1 = Ti.UI.createAnimation({transition:tr1});
-				var animate2 = Ti.UI.createAnimation({transition:tr2});
-
-				e.source.animate(animate1);
-				
-				animate1.addEventListener('complete', function() {
-					e.source.animate(animate2);
-				});
-				animate2.addEventListener('complete', function() {
-					e.source.animate(animate1);
-				});
-			});
+			
+			images.push(img);
 
 			var rating = require('stars');
 			var stars = rating(data[i].rating, 90);
@@ -106,21 +158,30 @@ module.exports = function() {
 			smallView.add(smallLoading);
 			smallView.add(img);
 			smallView.add(stars);
-			//smallView.add(Ti.UI.createLabel({text:data[i].rating, top:90, font:{fontSize:12}}));
 			if (data[i].num != 1) {
 				var num_votes = String.format(L('num_votes'), data[i].num);
 			} else {
 				var num_votes = String.format(L('num_vote'), 1);
 			}
 			smallView.add(Ti.UI.createLabel({text:num_votes, top:115, font:{fontSize:12}}));
-			scrollView.add(smallView);
+			//scrollView.add(smallView);
+			view.add(smallView);
+			
+			setTimeout(function() {
+				editButton.opacity = 1;
+				canEdit = true;
+			}, 300);
 		}
-		scrollView.add(Ti.UI.createView({height:15, top:top + 140})); // Espacio al final del scrollview
-		win.add(scrollView);
+		view.add(Ti.UI.createView({height:15, top:top + 140})); // Espacio al final del scrollview
+		//scrollView.add(Ti.UI.createView({height:15, top:top + 140})); // Espacio al final del scrollview
+		//win.add(scrollView);
+		scrollableView.addView(view);
+		getData(getImages, page + 1);
+		win.add(scrollableView);
 	}
 	
 	var getData = require('bbdd/getData');
-	getData(getImages);
+	getData(getImages, 1);
 	
 	win.add(header);
 
