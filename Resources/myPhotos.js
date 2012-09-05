e90b7d2e5535a525fd4ac745875c9afbbcdfa521
Guille <guillermo@artvisual.net>
2012-09-05 10:49:12 +0200

    cambios

--- Resources/myPhotos.js
+++ Resources/myPhotos.js
@@ -3,8 +3,19 @@ module.exports = function() {
 	var win = Ti.UI.createWindow({
 		backgroundImage:'images/background.png'
 	});
+	
+	var scrollView = Ti.UI.createScrollView({
+		showVerticalScrollIndicator:true,
+		contentHeight:'auto',
+		top:40
+	});
 
 	var data = [
+		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
+		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
+		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
+		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
+		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
 		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
 		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
 		{image:'http://ohcasionbv.es/img/Property/a1f9ee9f-2e71-4c15-875a-299f3ca22707/f37b4bce-c808-406b-b19b-1242438b37cf,fitCrop,180,121.jpg'},
@@ -20,7 +31,7 @@ module.exports = function() {
 	var top = 10;
 	for (i in data) {
 		var left = 10 + (i % 3) * 110;
-		if (i % 3 === 0) {
+		if (i % 3 === 0 && i != 0) {
 			top += 110;
 		}
 		var img = Ti.UI.createImageView({
@@ -30,8 +41,10 @@ module.exports = function() {
 			top:top,
 			left:left
 		});
-		win.add(img);
+		scrollView.add(img);
 	}
+	
+	win.add(scrollView);
 
 	return win;
 
